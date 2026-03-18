716128c05d6e1c206c73b120dfe7327af4580215
Elis Tenório <elismaidi@gmail.com>
2026-03-18 18:13:17 -0300

    add thread bateria

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,66 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    static boolean pausaBateria = true;
+
+    public static void main(String[] args) {
+
+
+        Thread bateria = new Thread(() -> {
+
+            while(true){
+
+                if(pausaBateria){
+
+                    try{
+                        Thread.sleep(100);
+                    } catch (InterruptedException e){
+                        return;
+                    }
+
+                } else{
+
+                    System.out.println("Bateria tocando");
+
+                    try{
+                        Thread.sleep(500);
+                    } catch (InterruptedException e){
+                        return;
+                    }
+
+                }
+
+            }
+        });
+
+
+        bateria.start();
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while(true){
+
+            String status = scanner.nextLine();
+
+            if(status.equals("tocar")){
+                pausaBateria = false;
+            }
+
+            else if (status.equals("pausar")){
+                pausaBateria = true;
+            }
+
+            else if (status.equals("interromper")){
+                bateria.interrupt();
+                break;
+            }
+
         }
+
+
+
+
     }
 }
