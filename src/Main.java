2f4cf40efa7bfc21d1022d7628386347dea6344a
Aguinaldo Neto <aguinaldoanselmo5@gmail.com>
2026-03-18 21:06:29 -0300

    add: synchronize

--- src/Main.java
+++ src/Main.java
@@ -3,64 +3,55 @@ import java.util.Scanner;
 public class Main {
 
     static boolean pausaBateria = true;
+    static final Object lock = new Object();
 
     public static void main(String[] args) {
 
-
         Thread bateria = new Thread(() -> {
-
-            while(true){
-
-                if(pausaBateria){
-
-                    try{
-                        Thread.sleep(100);
-                    } catch (InterruptedException e){
-                        return;
+            while (true) {
+                synchronized (lock) {
+                    while (pausaBateria) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            return;
+                        }
                     }
-
-                } else{
-
-                    System.out.println("Bateria tocando");
-
-                    try{
-                        Thread.sleep(500);
-                    } catch (InterruptedException e){
-                        return;
-                    }
-
                 }
+                System.out.println("Badum-tssss");
 
+                try {
+                    Thread.sleep(500); //eh bom a gente fazer um calculo de bpm aqui pra a gente conseguir mudar o bpm depois
+                } catch (InterruptedException e) {
+                    return;
+                }
             }
         });
-
 
         bateria.start();
 
         Scanner scanner = new Scanner(System.in);
 
-
-        while(true){
-
+        while (true) {
             String status = scanner.nextLine();
 
-            if(status.equals("tocar")){
-                pausaBateria = false;
+            if (status.equals("tocar")) {
+                synchronized (lock) {
+                    pausaBateria = false;
+                    lock.notify();
+                }
             }
-
-            else if (status.equals("pausar")){
-                pausaBateria = true;
+            else if (status.equals("pausar")) {
+                synchronized (lock) {
+                    pausaBateria = true;
+                }
             }
-
-            else if (status.equals("interromper")){
-                bateria.interrupt();
+            else if (status.equals("interromper")) {
+                bateria.interrupt(); // Isso aqui manda a thread interromper, é necessário?
                 break;
             }
-
         }
 
-
-
-
+        scanner.close();
     }
 }
